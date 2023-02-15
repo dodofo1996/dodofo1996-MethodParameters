ffd99e37a34efadfefccc2b5a9731522ad344a6f
dodofo1996 <dodofo1996@gmail.com>
2023-02-15 17:58:31 +0000

    lab 3 method

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -7,8 +7,9 @@ public class Application {
      * The Lab.copy method should return the same value that was passed to it. This value will be printed here.
      */
     public static void main(String[] args) {
+        
         System.out.println("The Lab.copy method should return the same value that was passed to it.");
-        Lab parrot = new Lab();
+       Lab parrot = new Lab();
 
         System.out.println("Here's what Lab.copy returns when provided the value \"repeat after me\"");
         String val1 = "repeat after me";
